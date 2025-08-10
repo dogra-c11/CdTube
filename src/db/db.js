bb82c9617b2115c677194224f6a66dc00da3c14f
Chirag Dogra <cd.chiragdogra@gmail.com>
2025-08-10 19:58:00 +0530

    start server after db connection

--- src/db/db.js
+++ src/db/db.js
@@ -3,12 +3,20 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
-    console.log("MongoDB connected successfully. DB Host:", connectionInstance.connection.host);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_DB_URI}/${DB_NAME}`
+    );
+    console.log(
+      "MongoDB connected successfully. DB Host:",
+      connectionInstance.connection.host
+    );
   } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit process if unable to connect
+    console.error("MongoDB connection error:", error.message);
+    throw error; // Re-throw the error to be handled by the caller
   }
 };
 
 export default connectDB;
+
+// an advantage of using mongoose nosql over mysql is that we can add columns on the fly without having to change the schema and making migrations
+// This flexibility allows for rapid development and iteration, especially in projects where requirements may change frequently.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import connectDB from './db/db.js'; 
+import { app } from "./app.js";
+import connectDB from "./db/db.js";
 
-connectDB(); // Establish the database connection
+connectDB() // Establish the database connection
+  .then(() => {
+    console.log("Database connection established successfully.");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1); // Exit the process if the connection fails
+  });
